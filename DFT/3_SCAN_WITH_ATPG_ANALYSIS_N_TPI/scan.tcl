2f83d4457fb0ff4f3c635f3dfd1bc6e328020528
keyurd1998-sys <keyur.d1998@gmail.com>
2026-07-11 11:55:12 +0530

    modified:   DFT/3_SCAN_WITH_ATPG_ANALYSIS_N_TPI/scan.tcl

--- DFT/3_SCAN_WITH_ATPG_ANALYSIS_N_TPI/scan.tcl
+++ DFT/3_SCAN_WITH_ATPG_ANALYSIS_N_TPI/scan.tcl
@@ -96,7 +96,7 @@ define_test_clock \
 # Define Scan Chains
 # =========================================================
 
-set NUM_SCAN_CHAINS 4
+set NUM_SCAN_CHAINS 44
 
 for {set i 1} {$i <= $NUM_SCAN_CHAINS} {incr i} {
 create_port \
