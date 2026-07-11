461f4f55d6fffff7a0849bbb1c2e802a1e0a8e54
keyurd1998-sys <keyur.d1998@gmail.com>
2026-07-11 12:06:08 +0530

    ATPG 99.9%

---
+++ DFT/3_SCAN_WITH_ATPG_ANALYSIS_N_TPI/TOP_TP/atpg.cmd
@@ -0,0 +1,4 @@
+# Cadence Modus
+#   Cadence(R) Modus(TM) DFT Software Solution, Version 25.10-p027_1, built Mar 13 2025 (linux26_64)
+#   Date: Saturday, July 11, 2026
+set code [catch { source TOP_TP/runmodus.init.tcl ; source TOP_TP/runmodus.atpg.tcl ;}]; exit $code
